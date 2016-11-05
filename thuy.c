867933334b1d679757010ece7265f04c6f0a36c5
thao nguyen <thao@rumie.org>
2016-11-04 21:15:05 -0400

    first commit

--- thuy.c
+++ thuy.c
@@ -10,30 +10,68 @@
 #include <sys/resource.h>
 #include <pthread.h>
 
+// add element to end: end is the index of free spot
+// remove element from begin: begin is the index of first occupied spot
+// array: -----begin------end--------
 struct Job {
 	int thread;
-	int   created_at;
-	struct Job *next;
-};
+	int created_at;
+} job;
+
+float lambda, mu;
+int server_count, client_count;
+int begin, end, length;
+struct Job queue[1000];
+pthread_t *servers, *clients, clock_thread;
 
 void* client() {
-	printf("syst time for children:  microseconds\n");
+	printf("enqueue");
 
 	// enqueue
+	queue[end].created_at = (unsigned)time(NULL);
+	end = end + 1;
+
+}
+
+void* server() {
+	printf("dequeue");
+
+	// dequeue
+	if (length > 0) {
+		length = length - 1;
+		begin = begin + 1;
+	}
+}
+
+void* clock_fn() {
+	printf("clock tick");
 }
 
 int main(int argc, char **argv)
 {
 	// printf("%s: syst time for children: %d microseconds\n",argv[0],time);
-	int server_count = 2, client_count = 2;
-	pthread_t servers[server_count], clients[client_count];
-	float lambda = 0.005, mu = 0.01;
-	struct Job head, tail;
+	server_count = 2, client_count = 2;
+	servers = malloc(server_count*sizeof(pthread_t));
+	clients = malloc(client_count*sizeof(pthread_t));
+	begin = 0, end = 0, length = 0;
+	lambda = 0.005, mu = 0.01;
 
 	// create clients
 	int i;
 	for( i = 0; i < client_count; i = i + 1 ) {
-		int err = pthread_create(&(clients[i]), NULL, &client, NULL);
+		pthread_create(&(clients[i]), NULL, &client, NULL);
+		pthread_join(clients[i], NULL); 
 	}
+
+	// create servers
+	for( i = 0; i < server_count; i = i + 1 ) {
+		pthread_create(&(servers[i]), NULL, &servers, NULL);
+		pthread_join(servers[i], NULL); 
+	}
+
+	// create clock thread
+	// pthread_create(&clock_thread, NULL, &clock_fn, NULL);
+
+	return 0;
 }
 
