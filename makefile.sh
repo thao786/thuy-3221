6d74bf9c8cc695db5edfaa279a8b6711d1f3ee47
thao nguyen <thao@rumie.org>
2016-11-24 22:05:10 -0500

    set up

--- makefile.sh
+++ makefile.sh
@@ -1,2 +1,9 @@
-gcc nlcnt.c 
-./a.out
+gcc -c mapping.c 
+gcc -shared -o mapping.so mapping.c 
+
+gcc -c reading.c 
+gcc -shared -o reading.so reading.c 
+
+gcc -o nlcnt nlcnt.c -ldl
+
+./nlcnt
--- nlcnt.c
+++ nlcnt.c
@@ -1,13 +1,23 @@
- #include <stdio.h>
- #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
 
+int main (int argc, char *argv[]) {
+  char m[] = "mapping";
+  char* dlfile = malloc(20*sizeof(char));
+  void *handle;
+  int (*my_cnt)(char*);
 
+  if (strstr(m, "mapping") != NULL) {
+    dlfile = "./mapping.so";
+  } else {
+    dlfile = "./reading.so";
+  }
 
- int main (int argc, char *argv[])
- {
-   
-   printf ("new lines count: %d\n", 5 );
+  handle = dlopen(dlfile, RTLD_LAZY);
+  my_cnt = dlsym(handle, "my_cnt");
+  printf ("%d \n", (*my_cnt)("2.0"));
 
-   
-
- } 
+  dlclose(handle);
+}
