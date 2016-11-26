0c2463fb78a9a87dc997011de95bd6e191f3fde8
thao nguyen <thao@rumie.org>
2016-11-25 22:04:54 -0500

    mapping works

--- mapping.c
+++ mapping.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
 #include <fcntl.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 
 int my_cnt (char *filename) {
-	printf("hello \n");
+	int fd, offset = 9;
+    char *data;
+    struct stat sbuf;
+
+	if ((fd = open(filename, O_RDONLY)) == -1) {
+        perror("open error.");
+        exit(1);
+    }
+
+    if (stat(filename, &sbuf) == -1) {
+        perror("stat error.");
+        exit(1);
+    }
+
+	if ((data = mmap (0, sbuf.st_size, PROT_READ, MAP_SHARED, fd, 0)) == (caddr_t) -1)
+		err_sys ("mmap error.");
+
+    printf("byte at offset %d is '%c'\n", offset, data[offset]);
 
 	return strlen(filename);
-	return 4;
 }
